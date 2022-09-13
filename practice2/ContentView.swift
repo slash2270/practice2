e77cd4fa02877f32688727464b56452bc8837202
slash <tw027slash@gmail.com>
2022-09-13 16:45:44 +0800

    Initial Commit

---
+++ practice2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  practice2
+//
+//  Created by user on 2022/9/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ practice2/practice2App.swift
@@ -0,0 +1,17 @@
+//
+//  practice2App.swift
+//  practice2
+//
+//  Created by user on 2022/9/13.
+//
+
+import SwiftUI
+
+@main
+struct practice2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
